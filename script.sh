d17c63d2a20bd971704557c18d6521567091532e
YAroslav10Ti <yaroslavtitov10@gmail.com>
2025-06-18 19:55:13 +0000

    Add working workflow

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Docker контейнер успешно запущен!" > output.log
-echo "Время запуска: $(date)" >> output.log
-echo "Системная информация:" >> output.log
-uname -a >> output.log
+echo "Контейнер успешно запущен!" > output.log
+echo "Дата: $(date)" >> output.log
+echo "Система: $(uname -a)" >> output.log
 cat output.log
+cp output.log /app/output/output.log 2>/dev/null || true
